702c151d902c9d9a43de22fbb47e3a55418ec9aa
junani-macmini <junani-macmini@mini.local>
2026-06-10 11:21:38 +0900

    Auto refresh World Cup live data

--- live-data.js
+++ live-data.js
@@ -1,5 +1,5 @@
 window.WORLD_CUP_LIVE_DATA = {
-  "updatedAt": "2026-06-10T02:05:34+00:00",
+  "updatedAt": "2026-06-10T02:21:37+00:00",
   "source": {
     "name": "ESPN public scoreboard API",
     "worldCupUrl": "https://site.api.espn.com/apis/site/v2/sports/soccer/fifa.world/scoreboard?dates=20260611-20260719&limit=250",
@@ -1679,11 +1679,11 @@ window.WORLD_CUP_LIVE_DATA = {
       "name": "Iceland at Argentina",
       "shortName": "ISL @ ARG",
       "status": {
-        "name": "STATUS_FIRST_HALF",
-        "description": "First Half",
+        "name": "STATUS_HALFTIME",
+        "description": "Halftime",
         "completed": false
       },
-      "venue": null,
+      "venue": "Jordan-Hare Stadium",
       "home": {
         "code": "ARG",
         "name": "Argentina",
@@ -1705,8 +1705,8 @@ window.WORLD_CUP_LIVE_DATA = {
       "name": "Venezuela at Iraq",
       "shortName": "VEN @ IRQ",
       "status": {
-        "name": "STATUS_IN_PROGRESS",
-        "description": "In Progress",
+        "name": "STATUS_HALFTIME",
+        "description": "Halftime",
         "completed": false
       },
       "venue": "SeatGeek Stadium",
